9e9dc96edcbed67ff892dd7a4cc44cacfb724a8c
jorgec0-est <j.curivil01@ufromail.cl>
2026-06-16 22:00:00 -0400

    Segunda prueba desde Windows

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Variables identificadas
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        menu();
+
+
+
+
+    }
+
+
+    public static void menu(){
+        // Probando menu
+        System.out.println("Menú principal");
+        System.out.println("seleccione opción");
+        Scanner leer = new Scanner(System.in);
+        int opc = leer.nextInt();
+        switch (opc){
+            case 1-> System.out.println("Esto es una prueba");
+            case 2-> System.out.println("1");
+            case 3-> System.out.println("2");
+            case 4-> System.out.println("3");
+            default -> System.out.println("Error");
+
         }
+
     }
 }
