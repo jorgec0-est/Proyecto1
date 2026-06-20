0484c9a142f1f08f1a4835f2186f0d211c4e8e43
jorgec0-est <j.curivil01@ufromail.cl>
2026-06-20 13:05:03 -0400

    Todo el flujo principal

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,13 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         // Variables identificadas
-
-        menu(); // Aquí comienza todo
-    }
-
-    // Hola esto es una
-
-    //
-
-
-    public static void menu(){
-        // Probando menu
         System.out.println("Menú básico");
         System.out.println("seleccione opción");
         Scanner leer = new Scanner(System.in);
         int opc;
 
         do {
-            System.out.println("\nMenú");
+            System.out.println("Menú");
             System.out.println("1. Opción 1");
             System.out.println("2. Opción 2");
             System.out.println("3. Salir");
@@ -34,6 +23,6 @@ public class Main {
                 default -> System.out.println("Opción inválida");
             }
         } while (opc != 3);
-
     }
+
 }
