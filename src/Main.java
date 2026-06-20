d63b302e6977b4dc14ea417d7ae28015b636ecf0
jorgec0-est <j.curivil01@ufromail.cl>
2026-06-20 13:49:27 -0400

    S

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,34 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         // Variables identificadas
-        System.out.println("Menú básico");
-        System.out.println("seleccione opción");
         Scanner leer = new Scanner(System.in);
+
+
+        // Empieza código
+        System.out.println("Bienvenido a la biblioteca de FROU");
         int opc;
 
         do {
-            System.out.println("Menú");
-            System.out.println("1. Opción 1");
-            System.out.println("2. Opción 2");
-            System.out.println("3. Salir");
+            System.out.println("Seleccione opción");
+            System.out.println(" ");
+            System.out.println("1. Registrar libro");
+            System.out.println("2. Ver catálogo");
+            System.out.println("3. Prestar libros");
+            System.out.println("4. Devoluciones de libros");
+            System.out.println("5. Buscar libros");
             System.out.print("> ");
 
             opc = leer.nextInt();
             switch (opc) {
-                case 1 -> System.out.println("Elegiste la opción 1");
-                case 2 -> System.out.println("Elegiste la opción 2");
-                case 3 -> System.out.println("Saliendo...");
+                case 1 -> System.out.println("Registra el libro");
+                case 2 -> System.out.println("Ver catálogo");
+                case 3 -> System.out.println("Prestar libros");
+                case 4 -> System.out.println("Devolviendo libro");
+                case 5 -> System.out.println("Busca el libro");
+                case 6 -> System.out.println("Saliendo . . .");
                 default -> System.out.println("Opción inválida");
             }
-        } while (opc != 3);
+        } while (opc != 6);
     }
 
 }
